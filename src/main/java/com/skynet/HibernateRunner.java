3264707ba6d91d02fea6791dbe1f7dda7c14b164
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2024-02-16 21:29:54 +0100

    feat: 02.Entity

--- src/main/java/com/skynet/HibernateRunner.java
+++ src/main/java/com/skynet/HibernateRunner.java
@@ -1,14 +1,17 @@
 package com.skynet;
 
 
+import com.skynet.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.SQLException;
+import java.time.LocalDate;
+
 
 public class HibernateRunner {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 //        BlockingDeque<Connection> poll = null;
 //         Connection connection = poll.take();
 //        SessionFactory
@@ -17,11 +20,25 @@ public class HibernateRunner {
 //                .getConnection("db.url", "db.username", "db.password");
 
         Configuration configuration = new Configuration();
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+//        configuration.addAnnotatedClass(User.class);
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+             User user = User.builder()
+                     .username("alex@gmail.com")
+                     .firstname("Alex")
+                     .lastname("Ali")
+                     .birthDate(LocalDate.of(1999, 12, 31))
+                     .age(18)
+                    .build();
+
+            session.save(user);
+//            session.persist(user);
+            session.getTransaction().commit();
         }
     }
 }
