6b860dbc3568d42f2af56cbf574c6dc42ca588ed
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2024-03-01 16:39:45 +0100

    feat: 16.FetchTypes

--- src/main/java/com/skynet/entity/User.java
+++ src/main/java/com/skynet/entity/User.java
@@ -1,10 +1,7 @@
 package com.skynet.entity;
 
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
@@ -14,6 +11,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "company")
 @Builder
 @Entity
 @Table(name = "users", schema = "public")
@@ -37,7 +35,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @ManyToOne
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id")
     private Company company;
 }
